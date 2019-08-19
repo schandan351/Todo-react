2b8199031a48405ab0ff15ebe1616dcd8e4dfac8
chandan sharma <sharmachandan351@gmail.com>
2019-08-19 23:37:55 +0545

    TodoInput,TodoItem,TodoList components are added and basic components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React ,{Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
+import TodoList from './components/TodoList';
 
 export default class App extends Component{
   render(){
     return(
-      <h1>Hello from app</h1>
-    )
+      <div>
+        <h1 className="bg-danger">Hello from app</h1>
+        <TodoInput />
+        <TodoItem />
+        <TodoList />
+      </div>
+    );
   }
 }
