c79a5ab981014cbab1ed1559e14f3dec3f0e8edb
chandan sharma <sharmachandan351@gmail.com>
2019-08-19 23:38:36 +0545

    TodoInput,TodoItem,TodoList components are added and basic components created

---
+++ src/components/TodoInput.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class TodoInput extends Component{
+  render(){
+    return(
+      <h2>Hey i' from todoinput</h2>
+    )
+  }
+}
---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class TodoItem extends Component{
+  render(){
+    return(
+      <h2>Hey i' from TodoItem</h2>
+    )
+  }
+}
