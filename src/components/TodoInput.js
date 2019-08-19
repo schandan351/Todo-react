1ba37d180e685a7acddfa44e7768e267258f1f9e
chandan sharma <sharmachandan351@gmail.com>
2019-08-20 00:57:31 +0545

    todoinput component added

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,9 +1,24 @@
 import React,{Component} from 'react';
-
+import {FaBook} from 'react-icons/fa';
 export default class TodoInput extends Component{
   render(){
+    const {item,handleChange,handleSubmit}=this.props;
     return(
-      <h2>Hey i' from todoinput</h2>
-    )
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <div className="input-group-text bg-primary text-white">
+                    <FaBook />
+                </div>
+              </div>
+              <input type="text" className="form-control text-capitalize" 
+              value={item} placeholder="Add todo items" onChange={handleChange}
+              />
+            </div>
+            <button className="btn btn-block btn-primary mt-3">Add todo</button>
+        </form>
+      </div>
+    );
   }
 }
